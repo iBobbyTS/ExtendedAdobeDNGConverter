4dceeab5e5a52158f1979e88560d8c9a9631dd76
iBobbyTS <ibobbyts@gmail.com>
2024-12-12 23:15:53 -0700

    Add requirements.txt

--- build_windows.sh
+++ build_windows.sh
@@ -1,1 +1,2 @@
+pip install -r requirements.txt
 flet pack main.py --add-data "config.json:." --add-data "languages.csv:." --add-data "exiftool/windows/exiftool.exe:exiftool/windows" --product-name "Extended Adobe DNG Converter" --bundle-id "com.ibobby.extendedAdobeDngConverter"
